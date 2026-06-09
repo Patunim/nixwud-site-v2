f5cc0ad9602b733d87528fca917f68fbdcd23b85
patunim <patrickunim@gmail.com>
2026-06-09 13:28:36 +0100

    update robot.txt file

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -4,10 +4,35 @@ export async function GET() {
 
   const robots = `
 User-agent: *
-
 Allow: /
 
-Sitemap: ${SITE_URL}/sitemap.xml
+Sitemap: ${SITE_URL}/sitemap-index.xml
+
+# AI + crawler controls
+
+User-agent: GPTBot
+Allow: /
+
+User-agent: Google-Extended
+Disallow: /
+
+User-agent: ClaudeBot
+Allow: /
+
+User-agent: CCBot
+Disallow: /
+
+User-agent: Bytespider
+Disallow: /
+
+User-agent: Applebot-Extended
+Disallow: /
+
+User-agent: Amazonbot
+Disallow: /
+
+User-agent: meta-externalagent
+Disallow: /
 `;
 
   return new Response(robots.trim(), {
